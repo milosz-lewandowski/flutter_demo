2b22f1dd5f3422081207d9499bbdd313f0a31b45
Miłosz Lewandowski <milo.lewandowski@gmail.com>
2023-01-12 21:38:20 +0100

    Markdown Screen

--- lib/MarkdownScreen.dart
+++ lib/MarkdownScreen.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:flutter_markdown/flutter_markdown.dart' as md;
+import 'package:flutter_markdown/flutter_markdown.dart';
 
 class MarkdownScreen extends StatefulWidget {
   const MarkdownScreen({super.key, required this.mdContentUrl});
+
   final String mdContentUrl;
 
   @override
   State<MarkdownScreen> createState() => _MarkdownScreenState();
-
 }
 
 class _MarkdownScreenState extends State<MarkdownScreen> {
-  late Future<md.Markdown> mdContent;
+  late Future<String> mdContent;
 
   @override
   void initState() {
@@ -22,26 +22,28 @@ class _MarkdownScreenState extends State<MarkdownScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<md.Markdown>(
-        future: mdContent,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return md.Markdown(data: snapshot.data!.toString());
-          } else if (snapshot.hasError) {
-            return md.Markdown(data: '${snapshot.error}');
-          }
-          return const CircularProgressIndicator();
-        }
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Markdown Sample"),
+        ),
+        body: FutureBuilder(
+            future: mdContent,
+            builder: (context, AsyncSnapshot<String> snapshot) {
+              if (snapshot.hasData) {
+                return Markdown(data: snapshot.data.toString());
+              } else if (snapshot.hasError) {
+                return Text('${snapshot.error}');
+              }
+              return const CircularProgressIndicator();
+            }));
   }
 
-  Future<md.Markdown> fetchMdContent() async {
+  Future<String> fetchMdContent() async {
     final response = await http.get(Uri.parse(widget.mdContentUrl));
     if (response.statusCode == 200) {
-      return md.Markdown(data: response.body);
+      return response.body;
     } else {
-      return const md.Markdown(data: "##Failed to load data");
+      return "##Failed to load data";
     }
   }
-
 }
